30b2e6657b19fcf86e04c1c4087fe645332498d0
Omidf1381 <77213717+Omidf1381@users.noreply.github.com>
2024-07-29 17:09:53 -0400

    added productController

---
+++ Controller/productController.js
@@ -0,0 +1,23 @@
+const Product = require('../models/productModel');
+
+exports.getProducts = async (req, res) => {
+  try {
+    const { name, price, category } = req.query;
+    let filter = {};
+
+    if (name) {
+      filter.product_name = { $regex: name, $options: 'i' };
+    }
+    if (price) {
+      filter.price = { $lte: price };
+    }
+    if (category) {
+      filter.category = category;
+    }
+
+    const products = await Product.find(filter).limit(20);
+    res.render('home', { products });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
