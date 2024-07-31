779f8312fcf2e262d3224c1c239b1116eb8f9bba
Malik Samouh <47095771+MalikSamouh@users.noreply.github.com>
2024-07-31 14:04:13 -0400

    user operations

--- Controller/productController.js
+++ Controller/productController.js
@@ -1,4 +1,4 @@
-const Product = require('../models/productModel');
+const Product = require('../Model/productModel');
 
 exports.getProducts = async (req, res) => {
   try {
--- Model/userModel.js
+++ Model/userModel.js
@@ -21,7 +21,6 @@ const userSchema = new mongoose.Schema({
   },
   address: {
     type: String,
-    required: true,
   },
 });
 
